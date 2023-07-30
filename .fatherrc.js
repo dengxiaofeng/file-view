4e78963a6ab352901b510687451df380746e950c
dengxiaofeng <1186313486@qq.com>
2023-07-30 16:49:32 +0800

    update version 0.0.3

--- .fatherrc.js
+++ .fatherrc.js
@@ -2,12 +2,7 @@ const path = require('path')
 const { defineConfig } = require('father')
 
 const options = {
-  esm: {
-    output: "dist",
-    input: 'src', // 默认编译目录
-    platform: 'browser', // 默认构建为 Browser 环境的产物
-    transformer: 'babel', // 默认使用 babel 以提供更好的兼容性
-  },
-  umd: { output: 'dist' }
+  esm: {},
+  umd: {}
 }
 export default options;
